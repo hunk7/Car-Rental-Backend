fd4f5ff428e3925e9f54203f2bdf93337283b7a0
Ganesh Bhadra <39549618+hunk7@users.noreply.github.com>
2020-05-05 23:25:39 +0530

    Update booking.js

--- models/booking.js
+++ models/booking.js
@@ -5,7 +5,7 @@ const bookSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    contactnumber:{
+    contactnum:{
         type:String,
         required:true
     },
